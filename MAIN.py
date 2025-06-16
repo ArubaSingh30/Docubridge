1bcf0144bb2e0313ad95e8db0cf81531a83add2b
sebibacila <119106840+sebibacila@users.noreply.github.com>
2025-06-16 19:28:17 +0300

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,5 +1,8 @@
 from types import MethodDescriptorType
 from flask import Flask, render_template, request
+import pandas
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -18,13 +21,25 @@ def upload():
 
     if file.filename == '':
         return 'No selected file', 400
+    
+    xls = pd.ExcelFile(file, engine='openpyxl')
+    sheets = xls.sheet_names
 
+    df = pd.read_excel(file, engine='openpyxl', sheet_name=0)
+    df.dropna(how='all', inplace=True)
+
+    #col2_sum = df.iloc[:, 1].sum()
+    
+    table_html = df.head(10).to_html(classes='table table-striped', index=False)
+    
     return render_template(
-          'result.html',
-          filename=file.filename,
-          question=question
+        'result.html',
+        filename=file.filename,
+        question=question,
+        sheets=sheets,
+        #col2_sum=col2_sum,
+        table_html=table_html
     )
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
